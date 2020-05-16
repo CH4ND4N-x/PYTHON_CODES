25307e9ee3a614a9ce860910c43f8da5da0e9a1d
Chandan Kumar Jangid <chandanjangid491@gmail.com>
2020-05-16 18:00:49 +0530

    new codes

--- instagram_login_logout.py
+++ instagram_login_logout.py
@@ -6,21 +6,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 #log_in details
-user_name="username"
-password="password"
+user_name="USER NAME"
+password="PASSWORD"
 
 #INSTAGRAM URL
 URL="https://www.instagram.com/?hl=en"
 # Browser : Firefox.
-driver=webdriver.Firefox(executable_path="path to your geckodriver.exe")
+driver=webdriver.Firefox(executable_path="PARTH TO geckodriver")
 driver.get(URL)
 time.sleep(3)
 
 #log In
 driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(user_name)
-time.sleep(1)
+time.sleep(5)
 driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(password)
-time.sleep(3)
+time.sleep(5)
 driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
 time.sleep(5)
 driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
